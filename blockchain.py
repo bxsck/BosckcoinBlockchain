ece137f6331dd3ead9ca5b7de151a0b6dceb5838
bxsck <bosstk05nya@gmail.com>
2021-04-11 00:17:11 +0700

    add create_block func

---
+++ blockchain.py
@@ -0,0 +1,23 @@
+#install
+#pip install flask==0.12.2
+
+#import
+import  datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+#building
+class Blockchain:
+    
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof=1,previous_hash='0')
+        
+    def create_block(self,proof,previous_hash):
+        block ={'index': len(self.chain)+1,
+                'timestamp': str(datetime.datetime.now()),
+                'proof':proof,
+                'previous_hash':previous_hash}
+        self.chain.append(block)
+        return block
