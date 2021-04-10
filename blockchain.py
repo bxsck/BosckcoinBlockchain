c11caaed0ccad89cc801dcfc36e31ffce3a6aaf1
bxsck <bosstk05nya@gmail.com>
2021-04-11 01:05:18 +0700

    add get whole chain

--- blockchain.py
+++ blockchain.py
@@ -76,4 +76,10 @@ def mine_block():
                 'proof': block['proof'],
                 'previous-hash':block['previous_hash']}
     return jsonify(response), 200
-    
+
+#Getting the full blockchain
+@app.route('/get_chain', mothods=['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'length': len(blockchain.chain)}
+    return jsonify(response), 200
