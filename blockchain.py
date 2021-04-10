9d862ac1ea39cf7b7498ad52bba938ef4e28a1a7
bxsck <bosstk05nya@gmail.com>
2021-04-11 01:42:03 +0700

    add isValid req

--- blockchain.py
+++ blockchain.py
@@ -14,11 +14,11 @@ class Blockchain:
         self.chain = []
         self.create_block(proof=1,previous_hash='0')
         
-    def create_block(self,proof,previous_hash):
-        block ={'index': len(self.chain)+1,
-                'timestamp': str(datetime.datetime.now()),
-                'proof':proof,
-                'previous_hash':previous_hash}
+    def create_block(self, proof, previous_hash):
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash}
         self.chain.append(block)
         return block
     
@@ -30,10 +30,10 @@ class Blockchain:
         check_proof = False
         while check_proof is False:
             hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
-            if (hash_operation[:4]=='0000'):
-                check_proof == True
+            if hash_operation[:4] == '0000':
+                check_proof = True
             else:
-                new_proof +=1
+                new_proof += 1
         return new_proof
     
     def hash(self, block):
@@ -45,12 +45,12 @@ class Blockchain:
         block_index = 1
         while block_index < len(chain):
             block = chain[block_index]
-            if block['previous_hash'] != self.hash():
+            if block['previous_hash'] != self.hash(previous_block):
                 return False
             previous_proof = previous_block['proof']
             proof = block['proof']
             hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()
-            if hash_operation[:4] != '0000' :
+            if hash_operation[:4] != '0000':
                 return False
             previous_block = block
             block_index += 1
@@ -72,14 +72,26 @@ def mine_block():
     block = blockchain.create_block(proof, previous_hash)
     response = {'message' : 'Congratulation, you just mined a block!',
                 'index' : block['index'],
-                'timestamp' : block['timestamps'],
+                'timestamp' : block['timestamp'],
                 'proof': block['proof'],
                 'previous-hash':block['previous_hash']}
     return jsonify(response), 200
 
 #Getting the full blockchain
-@app.route('/get_chain', mothods=['GET'])
+@app.route('/get_chain', methods=['GET'])
 def get_chain():
     response = {'chain': blockchain.chain,
                 'length': len(blockchain.chain)}
     return jsonify(response), 200
+
+#check is the chain valid
+@app.route('/is_valid', methods=['GET'])
+def is_valid():
+    is_valid=blockchain.is_chain_valid()
+    if is_valid :
+        response={'message':'The chain is valid'}
+    else :
+        response={'message':'The chain is not valid !!!!'}
+    return jsonify(response), 200
+#run
+app.run(host='0.0.0.0',port=5000)
