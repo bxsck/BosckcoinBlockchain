612a5d92c028143d726f8afa3f7a0755a69eaf1a
bxsck <bosstk05nya@gmail.com>
2021-04-11 00:17:42 +0700

    add get previous block

--- blockchain.py
+++ blockchain.py
@@ -21,3 +21,6 @@ class Blockchain:
                 'previous_hash':previous_hash}
         self.chain.append(block)
         return block
+    
+    def get_previous_block(self):
+        return self.chain[-1]
